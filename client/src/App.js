b6dc964851cf1e5a5d6dda33ab03c8e9d44aa197
ElizLongart <longartcoll@gmail.com>
2022-10-26 23:14:30 -0500

    initial frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Pokemon</h1>
-    </div>
+    <>
+      <BrowserRouter>   
+          <div className="App">
+            <h1>Henry Pokemon</h1>
+          </div>
+      </BrowserRouter> 
+    </>
   );
 }
 
