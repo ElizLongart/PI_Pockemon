93f7d23280f444a199558a3c2a253e4c4e8d34df
ElizLongart <longartcoll@gmail.com>
2022-10-26 19:16:01 -0500

    models + route pokemons

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -7,6 +7,71 @@ module.exports = (sequelize) => {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+      unique: true,
+      },
+    Id:{
+      type: DataTypes.UUID, //Me crea un número random con letras y numeros que es específico y no se repite
+      defaultValue: DataTypes.UUIDV4, //aleatorio
+      allowNull:false, // es para que si o sí tenga un id, al decir false me exige que tenga un id
+      primaryKey:true,
     },
-  });
+    image: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2017/07/247161-nuevos-pokemon-salen-huevos.jpg?itok=_KF-nmwE'
+    },
+    hp: {
+      type: DataTypes.INTEGER,
+      defaultValue: 55,
+      validate: {
+        max: 150,
+        min: 15
+      }
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      defaultValue: 48,
+      validate: {
+        max: 150,
+        min: 10
+      }
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      defaultValue: 65,
+      validate: {
+        max: 150,
+        min: 15
+      }
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      defaultValue: 45,
+      validate: {
+        max: 150,
+        min: 10
+      }
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      defaultValue: 2,
+      validate: {
+        max: 10,
+        min: 1
+      }
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      defaultValue: 300,
+      validate: {
+        max: 1500,
+        min: 1
+      }
+    },  
+    createdInDb: { //Para cuando quiero hacer un llamado unicamente a los que estan en db
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,    
+    }
+  },
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
+const { Pokemon, Type } = require('../db');
+const axios = require('axios');
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -6,7 +9,82 @@ const { Router } = require('express');
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
+
+const getApiInfo = async () => {
+
+	const pokemonsData = [];
+	const firstApi = await axios.get('https://pokeapi.co/api/v2/pokemon');
+	const partTwo = await axios.get(firstApi.data.next);
+	const urlFirstsPoke = firstApi.data.results.map(async(pokemon)=> await axios.get(pokemon.url));
+	const urlSecondsPoke = partTwo.data.results.map(async(pokemon)=> await axios.get(pokemon.url));
+	const urlAll = urlFirstsPoke.concat(urlSecondsPoke);
+
+	const pokeData = Promise.all(urlAll)
+		.then(poke=> {poke.map(p=>{
+			pokemonsData.push({
+				id: p.data.id,
+				name: p.data.name,
+				image: p.data.sprites.other.home.front_default,
+				types: p.data.types.map((type)=>{ return {'name': type.type.name}}),
+				attack: p.data.stats[1]['base_stat']
+			})
+		})
+		return pokemonsData;
+	})
+	return pokeData;
+
+/* 	const apiUrl = await axios.get('https://pokeapi.co/api/v2/pokemon');
+	const apiInfo = await apiUrl.data.map(el  =>{
+		return {
+			ID: el.data.id,
+			name: el.data.name,
+			image: el.data.sprites.other.home.front_default,
+			types: el.data.types.map((type)=>{ return {'name': type.type.name}}),
+			attack: el.data.stats[1]['base_stat']
+		};
+	});
+	return apiInfo; */
+};
+
+const getDbInfo = async () => {
+	const pokemonDb = await Pokemon.findAll({
+		include:{
+			model: Type, //incluye el modelo Type y traiga todos los personajes.
+			attributes: ['name'], // Del modelo Type, quiero que me traiga el nombre
+			through: {
+				attributes: [], //Mediante los atributos 
+			}
+		}
+	});
+	return pokemonDb;	
+};
+
+const getAllPokemons = async () => {
+	const apiPokemon = await getApiInfo();
+	const dbPokemon = await getDbInfo();
+	const allPokemons = apiPokemon.concat(dbPokemon);
+	return allPokemons
+};
+
+
+
+router.get('/pokemons', async (req, res) => {
+	const name  = req.query.name
+	
+ 	try {
+		let allPokemones = await getAllPokemons();
+		if (name) {
+		const pokemon = allPokemones.filter((pokemon)=> pokemon.name.toLowerCase() === name.toLowerCase())
+				if (pokemon[0]) return res.json(pokemon);
+				//acá buscar en la api por nombre
+				return res.status(404).send("Upps!! We can't find your pokemon")
+	} else {			
+			res.json(allPokemones);
+		}
+		} catch(e) {
+			res.status(400).send(e);		
+		} 
+});
 
 
 module.exports = router;
